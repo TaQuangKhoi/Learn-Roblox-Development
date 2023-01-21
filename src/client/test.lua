47ac435ccfc934d8dde09c934043733eeec4296a
TaQuangKhoi <khoi0941@gmail.com>
2023-01-21 20:23:47 +0700

    save

---
+++ src/client/test.lua
@@ -0,0 +1,1 @@
+print("Thu nghiem")
---
+++ src/server/test.lua
@@ -0,0 +1,8 @@
+print("Thu nghiem ne")
+
+-- code for Roblox
+-- Path: src\server\test.lua
+-- Compare this snippet from src\shared\Hello.lua:
+return function()
+    print("Hello, world!")
+end
